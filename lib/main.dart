a8425f8c63f573ff31867c4e2d47f4ecdfc30e31
Jefferson Li <jl@Jeffersons-MacBook-Pro.local>
2024-06-14 15:59:35 -0700

    Initial imp of map page

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,53 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'views/signup_step1_view.dart';
-import 'views/signup_step2_view.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'views/signup_step1_view.dart';
+// import 'views/signup_step2_view.dart';
 
-Future<void> main() async {
-  await dotenv.load(fileName: ".env");
-  runApp(MyApp());
+// Future<void> main() async {
+//   await dotenv.load(fileName: ".env");
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'ExpenseTrack',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       initialRoute: '/signup_step1',
+//       routes: {
+//         '/signup_step1': (context) => SignUpStep1View(),
+//         '/signup_step2': (context) => SignUpStep2View(),
+//       },
+//     );
+//   }
+// }
+
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'viewmodels/map_view_model.dart';
+import 'views/map_screen.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => MapViewModel(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ExpenseTrack',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/signup_step1',
-      routes: {
-        '/signup_step1': (context) => SignUpStep1View(),
-        '/signup_step2': (context) => SignUpStep2View(),
-      },
+      title: 'Map App',
+      home: MapScreen(),
     );
   }
 }
+
---
+++ lib/viewmodels/map_view_model.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../models/map_model.dart';
+
+class MapViewModel extends ChangeNotifier {
+  final List<Receipt> _receipts = [
+    Receipt('1', 'Macy\'s', DateTime(2024, 1, 2), 100.0, 'USD', Location(37.7749, -122.4194)),
+    Receipt('2', 'Car Rental', DateTime(2024, 1, 5), 200.0, 'USD', Location(37.7760, -122.4180)),
+    Receipt('3', 'Marriott', DateTime(2024, 1, 10), 300.0, 'USD', Location(37.7770, -122.4170)),
+    // Add more receipts with their respective locations
+  ];
+
+  List<Receipt> get receipts => _receipts;
+
+  Location _currentLocation = Location(37.7749, -122.4194); // San Francisco
+
+  Location get currentLocation => _currentLocation;
+
+  Receipt? _selectedReceipt;
+
+  Receipt? get selectedReceipt => _selectedReceipt;
+
+  void selectReceipt(Receipt receipt) {
+    _selectedReceipt = receipt;
+    notifyListeners();
+  }
+
+  void unselectReceipt() {
+    _selectedReceipt = null;
+    notifyListeners();
+  }
+}
